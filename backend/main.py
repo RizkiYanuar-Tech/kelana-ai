29e40fe14ad0b51211300684a0bd0bc613da26a6
Rizki Yanuar <ryanuar83@gmail.com>
2026-08-14 20:42:40 +0700

    Tambah logic get_travel_season

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+from services.trip_services import get_trip_category, calculate_daily_budget, get_transportation_recommendation, get_travel_season, recommended_places
+
 def print_trip_summary(destination, country, days, budget, currency, travel_month):
     print("="*20)
     print("KelanaAI")
@@ -8,13 +10,39 @@ def print_trip_summary(destination, country, days, budget, currency, travel_mont
     print(f"Budget: {budget}")
     print(f"Currency: {currency}")
     print(f"Travel Month: {travel_month}")
+    return destination, country, days, budget, currency, travel_month
 
 if __name__ == "__main__":
-    destinasi = str(input("Destination: "))
-    negara = str(input("Country: "))
+    destinasi = []
+    negara = []
+
+    while True:
+        user_destinasi = str(input("Destinasi: "))
+        if user_destinasi.lower() == 'exit':
+            break
+
+        destinasi.append(user_destinasi)
+
+    while True:
+        user_country = str(input("Country: "))
+        if user_country.lower() == 'exit':
+            break
+
+        negara.append(user_country)
+
     hari = int(input("Days: "))
     budget = float(input("Budget: "))
     mata_uang = str(input("Currency: "))
     travel_month = str(input("travel_month: "))
 
-    print_trip_summary(destinasi, negara, hari, budget, mata_uang, travel_month)
+    trip_summary = print_trip_summary(destinasi, negara, hari, budget, mata_uang, travel_month)
+    daily_budget = calculate_daily_budget(budget, hari)
+    category = get_trip_category(budget)
+    transport = get_transportation_recommendation(category)
+    season = get_travel_season(travel_month)
+
+    print(f"daily_budget: {daily_budget} {mata_uang}/day")
+    print(f"category: {category}")
+    print(f"transport: {transport}")
+    print(f"Seasons: {season}")
+    print(f"Recommended Places: {recommended_places}")
---
+++ backend/services/trip_services.py
@@ -0,0 +1,35 @@
+def get_trip_category(budget):
+    if budget < 1000:
+        category="Backpacker"
+    elif budget < 3000:
+        category="Standard"
+    else:
+        category="Luxury"
+    return category
+
+def get_travel_season(month):
+    if month.capitalize() == 'December':
+        return 'Peak Season'
+    elif month.capitalize() == 'June':
+        return 'Holiday Season'
+    else:
+        return 'Regular Season'
+
+def calculate_daily_budget(budget, days):
+    budget_daily =  budget / days
+    return budget_daily
+
+def get_transportation_recommendation(category):
+    if category == "Backpacker":
+        return "Bus"
+    elif category == "Standard":
+        return "Train"
+    else:
+        return "Flight"
+
+recommended_places = [
+        "shibuya", 
+        "tokyo tower",
+        "tokyo", 
+        "akihabara"
+    ]
